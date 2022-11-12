6f8ef36d5e765ce841fe0b32dafa1529884fc46a
Jaehoon You <teslamint@gmail.com>
2022-11-13 07:28:57 +0900

    apply zero-downtime script
    ref: https://www.tines.com/blog/simple-zero-downtime-deploys-with-nginx-and-docker-compose

--- update_akkoma.sh
+++ update_akkoma.sh
@@ -1,11 +1,44 @@
 #!/usr/bin/env bash
 set -x
+set -e
+set -u
+set -o pipefail
+
+reload_nginx() {  
+  docker-compose exec -T nginx /usr/sbin/nginx -s reload  
+}
+
+zero_downtime_deploy() {
+  service_name=web
+  old_container_id=$(docker ps -f name=$service_name -q | tail -n1)
+
+  # bring a new container online, running new code  
+  # (nginx continues routing to the old container only)  
+  docker-compose up -d --no-deps --scale $service_name=2 --no-recreate $service_name
+
+  # wait for new container to be available  
+  new_container_id=$(docker ps -f name=$service_name -q | head -n1)
+  new_container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $new_container_id)
+  curl --silent --include --retry-connrefused --retry 30 --retry-delay 1 --fail http://$new_container_ip:3000/ || exit 1
+
+  # start routing requests to the new container (as well as the old)  
+  reload_nginx
+
+  # take the old container offline  
+  docker stop $old_container_id
+  docker rm $old_container_id
+
+  docker-compose up -d --no-deps --scale $service_name=1 --no-recreate $service_name
+
+  # stop routing requests to the old container  
+  reload_nginx  
+}
+
 COMMIT_HASH=$(curl 'https://akkoma.dev/api/v1/repos/AkkomaGang/akkoma/branches/stable' | jq -r '.commit.id')
 COMMIT_ID=${COMMIT_HASH:-stable}
 
 docker buildx build --rm -t teslamint/akkoma:latest -t teslamint/akkoma:stable -t teslamint/akkoma:${COMMIT_ID} . --build-arg "PLEROMA_VER=$COMMIT_ID"
-# docker-compose run --rm web mix ecto.migrate
-docker-compose up -d
+zero_downtime_deploy
 
 # install frontends
 if [ ! -d static/frontends ]; then
