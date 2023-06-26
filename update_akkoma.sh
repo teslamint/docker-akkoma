c053b1b3a2be65f69d72582c9e3715651410d920
Jaehoon You <teslamint@gmail.com>
2023-06-27 08:33:24 +0900

    fix cannot reload nginx when index is changed

--- update_akkoma.sh
+++ update_akkoma.sh
@@ -1,30 +1,35 @@
 #!/usr/bin/env bash
 set -uo pipefail
 
+BRANCH=${BRANCH:-develop}
 DOCKER_COMPOSE=$(which docker-compose)
 if [[ "z$DOCKER_COMPOSE" = "z" ]]; then
   DOCKER_COMPOSE="docker compose"
 fi
 
-reload_nginx() {  
-  $DOCKER_COMPOSE exec -T nginx /usr/sbin/nginx -s reload  
+get_service_index() {
+  SERVICE_NAME=$1
+  $DOCKER_COMPOSE ps $SERVICE_NAME|tail -n1|awk '{print $1}'|sed -e s,akkoma-$SERVICE_NAME-,,
 }
 
-BRANCH=develop
+reload_nginx() {
+  NGINX_SERVICE_INDEX=$(get_service_index nginx)
+  $DOCKER_COMPOSE exec -T --index=$NGINX_SERVICE_INDEX nginx /usr/sbin/nginx -s reload
+}
 
 # check image already built
 $DOCKER_COMPOSE pull
 
 # docker rollout plugin required: https://github.com/Wowu/docker-rollout
 docker rollout web
-# stop routing requests to the old container  
+# stop routing requests to the old container
 reload_nginx
 
 # install frontends
 if [ ! -d pleroma/static/frontends ]; then
     mkdir -p pleroma/static/frontends
 fi
-SERVICE_INDEX=$($DOCKER_COMPOSE ps web|tail -n1|awk '{print $1}'|sed -e s,akkoma-web-,,)
+SERVICE_INDEX=$(get_service_index web)
 $DOCKER_COMPOSE exec -T --index=$SERVICE_INDEX web /pleroma/bin/pleroma_ctl frontend install pleroma-fe --ref ${BRANCH}
 $DOCKER_COMPOSE exec -T --index=$SERVICE_INDEX web /pleroma/bin/pleroma_ctl frontend install admin-fe --ref ${BRANCH}
 $DOCKER_COMPOSE exec -T --index=$SERVICE_INDEX web /pleroma/bin/pleroma_ctl frontend install mastodon-fe --ref akkoma
