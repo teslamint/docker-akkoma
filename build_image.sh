56faa1c27af6974bb1ae107904efeb267c628630
Jaehoon You <teslamint@gmail.com>
2023-09-28 21:38:54 +0900

    ci: build both

--- build_image.sh
+++ build_image.sh
@@ -4,13 +4,18 @@ set -e
 set -u
 set -o pipefail
 
-BRANCH=develop
+BRANCH=${BRANCH:-develop}
+IMAGE=${IMAGE:-teslamint/akkoma}
 COMMIT_HASH=$(curl "https://akkoma.dev/api/v1/repos/AkkomaGang/akkoma/branches/$BRANCH" | jq -r '.commit.id')
 COMMIT_ID=${COMMIT_HASH:-$BRANCH}
 
 # check image already built
-docker pull teslamint/akkoma:${BRANCH} || true
-docker pull teslamint/akkoma:${COMMIT_ID} || true
+docker pull ${IMAGE}:${BRANCH} || true
+docker pull ${IMAGE}:${COMMIT_ID} || true
 
-docker buildx build --rm -t teslamint/akkoma:latest -t teslamint/akkoma:${BRANCH} -t teslamint/akkoma:${COMMIT_ID} . \
+TAGS="-t ${IMAGE}:${BRANCH} -t ${IMAGE}:${COMMIT_ID}"
+if [[ "$BRANCH" = "develop" ]]; then
+    TAGS="-t ${IMAGE}:latest $TAGS"
+
+docker buildx build --rm $TAGS . \
     --build-arg "BRANCH=$BRANCH" --build-arg "PLEROMA_VER=$COMMIT_ID" --platform "linux/amd64,linux/arm64" --push
